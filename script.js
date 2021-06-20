0139b33c0c43e5f855e742f6ede034da79198c6a
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-06-20 15:22:48 -0400

    updated with subsequent prompts and reiteration of selections

--- script.js
+++ script.js
@@ -11,18 +11,52 @@ var passwordLength = "";
 var selectedspecilCharacter;
 var selectedupperCase;
 var selectedlowerCase;
+var selectednumbers;
 
 //first prompt to determine password length
 function generatePassword() {
     var passwordLength = (prompt("What length password are you looking to generate? (Select a length between 8 and 128 characters)"))
         // check for password length input
     if (passwordLength <= 7 || passwordLength >= 129) {
-        alert("Please enter a valid password Length  by choosing a number between 8 and 128 to determine character length of the password")
+        alert("Please enter a valid password Length  by choosing a number between 8 and 128 to determine character length of the password");
+        return;
     }
     // confirms length of password 
     else {
-        alert(`Your password will be ${passwordLength} characters long!`)
+        alert(`Please proceed with selecting what comprises your ${passwordLength} character long password! `)
     }
+
+    //subsequent prompts for including specialcharacters, uppercase and lowercase characters
+    var selectedspecilCharacter = confirm("Click OK if you would like to include special characters (@, !, ~, ...) in your randomly generated password");
+    var selectednumbers = confirm("Click OK if you would like to include numbers (0 -9) in your randomly generated password");
+    var selectedupperCase = confirm("Click OK if you would like to include upperCase characters (A, B,C, ....) in your randomly generated password");
+    var selectedlowerCase = confirm("Click OK if you would like to include lowerCase characters (a,b,c, ...) in your randomly generated password");
+
+    //Validating for atleast 1 prompt being okayed
+    while (selectedlowerCase === false && selectedupperCase === false && selectedspecilCharacter === false && selectednumbers === false) {
+        alert("Atleast one selection must be considered to create a randomly generated password");
+        var selectedspecilCharacter = confirm("Click OK if you would like to include special characters (@, !, ~, ...) in your randomly generated password");
+        var selectednumbers = confirm("Click OK if you would like to include numbers (0 -9) in your randomly generated password");
+        var selectedupperCase = confirm("Click OK if you would like to include upperCase characters (A, B,C, ....) in your randomly generated password");
+        var selectedlowerCase = confirm("Click OK if you would like to include lowerCase characters (a,b,c, ...) in your randomly generated password");
+    }
+
+    var selectedOptions = {
+            specialCharacters: selectedspecilCharacter,
+            numbers: selectednumbers,
+            lowerCase: selectedlowerCase,
+            upperCase: selectedupperCase
+        }
+        // return selectedOptions;
+
+    //reiterates user's selection prior to displaying th randomly generated password
+    if (selectedlowerCase === true || selectedupperCase === true || selectedspecilCharacter === true || selectednumbers === true) {
+        alert(`Your ${passwordLength} character long password will contain ${JSON.stringify(selectedOptions, null, 2)} `);
+    }
+    //empty array variable acting like a temp collector
+    var passwordChar = [];
+
+
 }
 
 // Write password to the #password input

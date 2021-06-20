b8ab60f81a5cc1daffc91e75d381d829943988cc
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-06-20 17:04:32 -0400

    got to a point of generating a password but it contains gibberish which needs cleaning

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", ",", "`", "{", "|", "}", "~"];
-
+let characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~`!@#$%^&*()_-+={[}]|:;<,>.?';
+let passwordValue = '';
+//password placeholders
+var passwordValuel;
+var passwordValuen;
+var passwordValues;
+var passwordValueu;
 var passwordLength = "";
 var selectedspecilCharacter;
 var selectedupperCase;
@@ -50,13 +56,43 @@ function generatePassword() {
         // return selectedOptions;
 
     //reiterates user's selection prior to displaying th randomly generated password
-    if (selectedlowerCase === true || selectedupperCase === true || selectedspecilCharacter === true || selectednumbers === true) {
-        alert(`Your ${passwordLength} character long password will contain ${JSON.stringify(selectedOptions, null, 2)} `);
+    // if (selectedlowerCase === true || selectedupperCase === true || selectedspecilCharacter === true || selectednumbers === true) {
+    //     alert(`Your ${passwordLength} character long password will contain ${JSON.stringify(selectedOptions, null, 2)} `);
+    // }
+
+    if (selectedupperCase) {
+        console.log(upperCase.join(","));
+        for (let i = 0; i < passwordLength; i++) {
+            let number = Math.floor(Math.random() * upperCase.length);
+            passwordValueu += upperCase.join(",").substring(number, number + 1); // adds the second character to first by appending it as opposed to replacing it and has converted everything to a string
+        }
     }
-    //empty array variable acting like a temp collector
-    var passwordChar = [];
+    if (selectedlowerCase) {
+        console.log(lowerCase.join(","));
+        for (let j = 0; j < passwordLength; j++) {
+            let number = Math.floor(Math.random() * lowerCase.length);
+            passwordValuel += lowerCase.join(",").substring(number, number + 1) // adds the second character to first by appending it as opposed to replacing it and has converted everything to a string
+        }
+    }
+    if (selectednumbers) {
+        console.log(numbers.join(","));
+        for (let k = 0; k < passwordLength; k++) {
+            let number = Math.floor(Math.random() * numbers.length);
+            passwordValuen += numbers.join(",").substring(number, number + 1) // adds the second character to first by appending it as opposed to replacing it and has converted everything to a string
+        }
+    }
 
+    if (selectedspecilCharacter) {
+        console.log(specialCharacters.join(","));
+        for (let l = 0; l < passwordLength; l++) {
+            let number = Math.floor(Math.random() * specialCharacters.length);
+            passwordValues += specialCharacters.join(",").substring(number, number + 1) // adds the second character to first by appending it as opposed to replacing it and has converted everything to a string
+        }
+    }
 
+    var randomPassword = passwordValuel + passwordValuen + passwordValueu + passwordValues;
+    alert("Your password is " + randomPassword);
+    console.log(randomPassword);
 }
 
 // Write password to the #password input
